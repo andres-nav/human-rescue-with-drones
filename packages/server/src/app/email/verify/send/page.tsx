8703453d35f76a0398f2d27cc53ae73794943f4b
Andrés Navarro Pedregal <contact@andresnav.com>
2024-10-06 17:43:48 +0200

    fix: add missing layout for verify email page

--- packages/server/src/app/email/verify/send/page.tsx
+++ packages/server/src/app/email/verify/send/page.tsx
@@ -1,13 +1,17 @@
 import Form from "@/components/email/verify/send/form";
 import { Suspense } from "react";
 
+import AuthLayout from "@/components/layouts/authLayout";
+
 export default function Send() {
   return (
-    <div className="flex flex-col">
-      <div className="mb-4">Please verify your email first.</div>
-      <Suspense>
-        <Form />
-      </Suspense>
-    </div>
+    <AuthLayout>
+      <div className="flex flex-col text-center">
+        <div className="mb-4">Please verify your email first.</div>
+        <Suspense>
+          <Form />
+        </Suspense>
+      </div>
+    </AuthLayout>
   )
 }
