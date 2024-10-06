06d87fb6836f54a1674fdf4d25c3639db2d89c48
Andrés Navarro Pedregal <contact@andresnav.com>
2024-10-06 17:43:04 +0200

    fix: style button for resend email verification

--- packages/server/src/components/email/verify/send/resend-button.tsx
+++ packages/server/src/components/email/verify/send/resend-button.tsx
@@ -1,7 +1,9 @@
 import { useFormStatus } from "react-dom"
 
+import { Button } from "@/components/ui/button"
+
 export default function ResendButton() {
     const { pending } = useFormStatus()
 
-    return <button type="submit" className="bg-white py-2 px-4 rounded disabled:bg-slate-50 disabled:text-slate-500" disabled={pending ? true : false}>Send verification link {pending ? '...' : ''}</button>
+    return <Button type="submit" className="bg-white text-black py-2 px-4 rounded disabled:bg-slate-50 disabled:text-slate-500" disabled={pending ? true : false}>Send verification link {pending ? '...' : ''}</Button>
 }
